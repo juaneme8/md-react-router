52bc0ec980095820687099899cb17ebd9020e610
Juan Ocho <juaneme8@gmail.com>
2020-11-05 01:35:42 -0300

    Using Router, Route and Switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+import Shop from './components/Shop';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Home = () => <h1>Home Page</h1>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Nav />
+			<Switch>
+				<Route path='/' exact component={Home} />
+				<Route path='/about' component={About} />
+				<Route path='/shop' component={Shop} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
