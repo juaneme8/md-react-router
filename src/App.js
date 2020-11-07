3a9b2fa300ee5671d69d24511a23557b1dac6b87
Juan Ocho <juaneme8@gmail.com>
2020-11-07 13:24:51 -0300

    Redirects

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import About from './components/About';
 import Shop from './components/Shop';
 import ItemDetails from './components/ItemDetails';
 import Products from './components/Products';
+import NotFound from './components/NotFound';
+import HomeButton from './components/HomeButton';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 const Home = () => <h1>Home Page</h1>;
 
 function App() {
 	return (
 		<Router>
+			<HomeButton />
 			<Nav />
 			<Switch>
 				<Route path='/' exact component={Home} />
@@ -18,6 +21,9 @@ function App() {
 				<Route path='/shop' exact component={Shop} />
 				<Route path='/shop/:id' component={ItemDetails} />
 				<Route path='/products' component={Products} />
+
+				<Route path='/not-found' component={NotFound} />
+				<Redirect to='/not-found' />
 			</Switch>
 		</Router>
 	);
